77f371cb3dcf4ba13f2ae8b3525685b8566cac2d
Ч-ƁƖƬɀ <a16100266@live.ceti.mx>
2021-05-21 22:27:17 -0500

    Primer commit
    Codigo Arduino desde devBitz

---
+++ BB_ArduinoCode/BB_ArduinoCode.ino
@@ -0,0 +1,13 @@
+//----THIS IS OUR ARDUINO CODE, ONE FOR ALL OF US----
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//----THIS IS OUR ARDUINO CODE, ONE FOR ALL OF US----
