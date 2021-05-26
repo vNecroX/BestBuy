d3eb82935f7fbe5bc4ade1f0112b899637d25d11
Ч-ƁƖƬɀ <a16100266@live.ceti.mx>
2021-05-25 22:50:26 -0500

    Comienzo de Forms
    Comunicacion entre ellos

---
+++ BestBuy_Proyecto/BestBuy_Proyecto/Dashboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BestBuy_Proyecto
+{
+    public partial class frm_Login : Form
+    {
+        public static Form dashboard;
+
+        public frm_Login()
+        {
+            InitializeComponent();
+            dashboard = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddWorker addWorker = new AddWorker(dashboard);
+            addWorker.Show();
+            this.Hide();
+        }
+    }
+}
